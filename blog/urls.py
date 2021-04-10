29fbf6d254844f2c45001b504b1aba1510a8300e
Kazim Muhammad <kazmodeston1@gmail.com>
2021-04-10 21:08:31 +0100

    included template path to DIRS under TEMPLATES in settings.py

--- blog/urls.py
+++ blog/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views as template
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', template.home),
+    path('about/', template.about),
 ]
